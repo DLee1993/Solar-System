e05f69ada049e6b6359158f4a7e6e9fa74ffebca
DLee1993 <Dai180293@gmail.com>
2022-04-11 22:30:41 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// This is used to import the data from the json file
+import data from "./data.json" assert { type: "json" };
+
+function dataLoop(planetName) {
+    for (var destinations in data) {
+        for (var planet in data[destinations]) {
+            if (data[destinations][planet].name === planetName) {
+                document.querySelector(".image").src = data[destinations][planet].images.png;
+            }
+        }
+    }
+}
+
+document.querySelectorAll("button").forEach(function (btn) {
+    btn.addEventListener("click", (e) => {
+        const planetName = e.target.innerHTML;
+        dataLoop(planetName);
+    });
+});
