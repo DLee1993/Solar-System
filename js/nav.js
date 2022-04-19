f87e05467b13b2e15dc26e3f6ccbb16ef77fbdb2
DLee1993 <Dai180293@gmail.com>
2022-04-19 12:06:11 +0100

    added gsap to all html files, gsap was used to animate the navbar

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,14 @@
 const hamburger = document.querySelector('.hamburger-icon'),
     closeIcon = document.querySelector('.close-icon'),
     navList = document.querySelector('.nav-list'),
+    navItems = document.querySelectorAll('.nav-item'),
     home = document.querySelector('.home'), 
     destinations = document.querySelector('.destinations'), 
     crew = document.querySelector('.crew'), 
-    technology = document.querySelector('.technology');
+    technology = document.querySelector('.technology'),
+    logo = document.querySelector('.logo-homeLink'),
+    styleLine = document.querySelector('.style-line'),
+    tl = gsap.timeline(); 
 
 let navOpen = false;
 
@@ -50,3 +54,10 @@ if(document.body.className){
             break;
     }
 }
+
+tl.to(logo, {opacity: 1})
+tl.to(styleLine, {left: '140px'})
+tl.to(navList, {height: '80px'}, '-=0.8')
+navItems.forEach(item => {
+    tl.to(item, {opacity: 1, y: -10}, '+=0.2')
+})
