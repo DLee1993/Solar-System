be3a58d3cda6a97289cc6c93c49ca0b9f28cc32f
DLee1993 <Dai180293@gmail.com>
2022-04-21 04:55:31 +0100

    added an animation timeline to the nav items

--- js/animations.js
+++ js/animations.js
@@ -1,36 +1,41 @@
 const logoIcon = document.querySelector(".logo-homeLink"),
-      menuIcon = document.querySelector('.hamburger-icon'),
-      designLine = document.querySelector(".style-line"),
-      navItemList = document.querySelector('.nav-list');
+    menuIcon = document.querySelector(".hamburger-icon"),
+    designLine = document.querySelector(".style-line"),
+    navItemList = document.querySelector(".nav-list"),
+    navItem = navItemList.querySelectorAll(".nav-item");
 
-
-
-
-var tl = gsap.timeline({duration: 0.3});
+var tl = gsap.timeline();
+var timeScale = tl.timeScale(1.2)
 
 const runMobileAnim = () => {
     tl.to(logoIcon, { x: 0, opacity: 1 });
-    tl.to(menuIcon, { y: 0, opacity: 1 }, '-=0.5');
-    tl.to(navItemList, {opacity: 1}, '-=0.5')
-}
+    tl.to(menuIcon, { y: 0, opacity: 1 }, "-=0.5");
+};
 
 const runTabletAnim = () => {
     tl.to(logoIcon, { x: 0, opacity: 1 });
-    tl.to(navItemList, {opacity: 1,}, '-=0.5');
+    tl.to(navItemList, { opacity: 1 });
+    navItem.forEach((item) => {
+        tl.to(item, { y: 0, opacity: 1 });
+    });
 };
 
 const runDesktopAnim = () => {
     tl.to(logoIcon, { x: 0, opacity: 1 });
-    tl.to(designLine, {opacity: 1, x: 0}, '-=0.5')
-    tl.to(navItemList, {opacity: 1,}, '-=0.3');
+    tl.to(designLine, { x: 0, opacity: 1 }, "-=3");
+    tl.to(navItemList, { opacity: 1 });
+    navItem.forEach((item) => {
+        tl.to(item, { y: 0, opacity: 1 });
+    });
+    console.log(tl.totalDuration());
 };
 
 const ready = () => {
     ScrollTrigger.matchMedia({
         "(max-width: 600px)": () => {
-            runMobileAnim(); 
+            runMobileAnim();
         },
-        "(min-width: 600px)": () => {
+        "(min-width: 601px)": () => {
             runTabletAnim();
         },
         "(min-width: 1100px)": () => {
