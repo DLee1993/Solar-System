1a99d00197d7112080b72e90cb50108bda079ba5
DLee1993 <Dai180293@gmail.com>
2022-04-29 04:08:38 +0100

    added a scrolltrigger match media to change the width and height of the explore cta button depending on the screen size

--- js/anims/indexContentAnim.js
+++ js/anims/indexContentAnim.js
@@ -7,4 +7,15 @@ const supheading = document.querySelector(".supheading"),
 indexTL.fromTo(supheading, {y: 20, opacity: 0}, {y: 0, opacity: 1});
 indexTL.fromTo(heading, {y: 20, opacity: 0}, {y: 0, opacity: 1});
 indexTL.fromTo(subheading, {y: 20, opacity: 0}, {y: 0, opacity: 1});
-indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '274px', height: '274px', ease: 'back.inOut(1.7)', duration: 1.2})
+
+ScrollTrigger.matchMedia({
+    "(max-width: 600px)": () => {
+        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '150px', height: '150px', ease: 'back.inOut(1.7)', duration: 1.2})
+    },
+    "(min-width: 601px) and (max-width: 1099px)": () => {
+        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '242px', height: '242px', ease: 'back.inOut(1.7)', duration: 1.2})
+    },
+    "(min-width: 1100px)": () => {
+        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '274px', height: '274px', ease: 'back.inOut(1.7)', duration: 1.2})
+    },
+});
