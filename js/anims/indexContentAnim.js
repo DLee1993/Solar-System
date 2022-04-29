553f3784c80062cc6a754c64be8ea0df2193f70a
DLee1993 <Dai180293@gmail.com>
2022-04-29 06:30:23 +0100

    removed the delay from the explore cta gsap timeline

--- js/anims/indexContentAnim.js
+++ js/anims/indexContentAnim.js
@@ -10,13 +10,13 @@ indexTL.fromTo(subheading, {y: 20, opacity: 0}, {y: 0, opacity: 1}, '-=0.3');
 
 ScrollTrigger.matchMedia({
     "(max-width: 600px)": () => {
-        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '150px', height: '150px', ease: 'back.inOut(1.7)', duration: 0.8}, '-=0.8')
+        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '150px', height: '150px', ease: 'back.inOut(1.7)', duration: 0.8})
     },
     "(min-width: 601px) and (max-width: 1099px)": () => {
-        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '242px', height: '242px', ease: 'back.inOut(1.7)', duration: 1.2}, '-=0.8')
+        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '242px', height: '242px', ease: 'back.inOut(1.7)', duration: 1.2})
     },
     "(min-width: 1100px)": () => {
-        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '274px', height: '274px', ease: 'back.inOut(1.7)', duration: 1.2}, '-=0.8')
+        indexTL.fromTo(exploreCTA, {width: 0, height: 0}, {width: '274px', height: '274px', ease: 'back.inOut(1.7)', duration: 1.2})
     },
 });
 
