f04d1dc5566a92a4db0437fd48dbc3fecf10e874
DLee1993 <Dai180293@gmail.com>
2022-05-02 06:25:41 +0100

    slight adjustment to delays and durations of animations

--- js/animations/destinationsAnimations.js
+++ js/animations/destinationsAnimations.js
@@ -1,5 +1,6 @@
-const planetsTL = gsap.timeline(),
-    planetImage = document.querySelector(".planet-image");
+const planetsTL = gsap.timeline({delay: 1.5}),
+    planetImage = document.querySelector(".planet-image"), 
+    pageTitle = document.querySelector('.page-title');
 
-
-planetsTL.fromTo(planetImage, {height: 0}, {height: '100%', duration: 2.5})
+planetsTL.fromTo(pageTitle, {opacity: 0, y: 10}, {opacity: 1, y: 0})
+planetsTL.fromTo(planetImage, {height: 0}, {height: '100%', ease: 'back.inOut(1.5)', duration: 1.5})
--- js/animations/indexContentAnim.js
+++ js/animations/indexContentAnim.js
@@ -5,8 +5,8 @@ const supheading = document.querySelector(".supheading"),
     indexTL = gsap.timeline({delay: 0.4, ease: 'power2.out'});
 
 indexTL.fromTo(supheading, {y: 20, opacity: 0}, {y: 0, opacity: 1});
-indexTL.fromTo(heading, {y: 20, opacity: 0}, {y: 0, opacity: 1}, '-=0.15');
-indexTL.fromTo(subheading, {y: 20, opacity: 0}, {y: 0, opacity: 1}, '-=0.3');
+indexTL.fromTo(heading, {y: 20, opacity: 0}, {y: 0, opacity: 1});
+indexTL.fromTo(subheading, {y: 20, opacity: 0}, {y: 0, opacity: 1}, '-=0.5');
 
 ScrollTrigger.matchMedia({
     "(max-width: 600px)": () => {
