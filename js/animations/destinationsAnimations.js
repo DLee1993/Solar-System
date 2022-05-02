1a7d56fe3889e1d023c4276f23ccdaf2e7eb6038
DLee1993 <Dai180293@gmail.com>
2022-05-02 17:55:45 +0100

    decreased the time between the title and planet image animations

--- js/animations/destinationsAnimations.js
+++ js/animations/destinationsAnimations.js
@@ -3,4 +3,4 @@ const planetsTL = gsap.timeline({delay: 1.2}),
     pageTitle = document.querySelector('.page-title');
 
 planetsTL.to(pageTitle, {opacity: 1, y: 0})
-planetsTL.to(planetImage, {height: '100%', ease: 'back.inOut(1.5)', duration: 1.5}, '-=0.5')
+planetsTL.to(planetImage, {height: '100%', ease: 'back.inOut(1.5)', duration: 1.5}, '-=0.8')
