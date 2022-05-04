862e6acd880e852bc8477339554b5283f076bccd
DLee1993 <Dai180293@gmail.com>
2022-05-04 14:06:20 +0100

    added html to destinations file

--- js/animations/destinationsAnimations.js
+++ js/animations/destinationsAnimations.js
@@ -2,5 +2,5 @@ const planetsTL = gsap.timeline({delay: 1.2}),
     planetImage = document.querySelector(".planet-image"), 
     pageTitle = document.querySelector('.page-title');
 
-planetsTL.to(pageTitle, {opacity: 1, y: 0})
-planetsTL.to(planetImage, {height: '100%', ease: 'back.inOut(1.5)', duration: 1.5}, '-=0.8')
+    planetsTL.to(pageTitle, {opacity: 1, y: 0});
+    planetsTL.to(planetImage, {height: '100%', ease: 'back.inOut(1.5)', duration: 1.5}, '-=0.8');
