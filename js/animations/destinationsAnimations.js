4371abb97ca550aad977cd546e6dd9bdff9fae57
DLee1993 <Dai180293@gmail.com>
2022-05-05 09:40:50 +0100

    removed destinations html

--- js/animations/destinationsAnimations.js
+++
@@ -1,6 +0,0 @@
-const planetsTL = gsap.timeline({delay: 1.2}),
-    planetImage = document.querySelector(".planet-image"), 
-    pageTitle = document.querySelector('.page-title');
-
-    planetsTL.to(pageTitle, {opacity: 1, y: 0});
-    planetsTL.to(planetImage, {height: '100%', ease: 'back.inOut(1.5)', duration: 1.5}, '-=0.8');
--- js/app.js
+++ js/app.js
@@ -7,16 +7,15 @@ function dataLoop(planetName) {
             if (data[destinations][planet].name === planetName) {
                 //*! Add all of the information within this loop, make sure to name everything generically as it will be used for all planets  */
                 //* Change the below .image to the class name of the planet img tag!!
-                document.querySelector(".planetImage").src = data[destinations][planet].images.png;
+                document.querySelector(".planet-image").src = data[destinations][planet].images.png;
             }
         }
     }
 }
 
-document.querySelectorAll("button").forEach(function (btn) {
+document.querySelectorAll(".planet-list li").forEach(function (btn) {
     btn.addEventListener("click", (e) => {
         const planetName = e.target.innerHTML;
         dataLoop(planetName);
-        console.log(document.querySelector('.planetImage').src)
     });
 });
