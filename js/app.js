b030a4c623eb2231b0fcecc14537b0aacaa4908a
DLee1993 <Dai180293@gmail.com>
2022-05-05 18:58:39 +0100

    added all html markup for destinations page, also added all json data to app.js

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ function dataLoop(planetName) {
                 //*! Add all of the information within this loop, make sure to name everything generically as it will be used for all planets  */
                 //* Change the below .image to the class name of the planet img tag!!
                 document.querySelector(".planet-image").src = data[destinations][planet].images.png;
+                document.querySelector('.planet-name').innerHTML = data[destinations][planet].name;
+                document.querySelector('.planet-info').innerHTML = data[destinations][planet].description;
+                document.querySelector('.distance-value').innerHTML = data[destinations][planet].distance;
+                document.querySelector('.travelTime-value').innerHTML = data[destinations][planet].travel;
             }
         }
     }
